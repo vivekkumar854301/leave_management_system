c6a8b8d412680b3118b28a73004a09c27f2f1399
vivekkumar854301 <viveksingh144399@gmail.com>
2025-03-27 01:17:11 +0530

    initial setup

---
+++ lms/src/app/shared/component/backbutton/backbutton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backbutton',
+  imports: [],
+  templateUrl: './backbutton.component.html',
+  styleUrl: './backbutton.component.scss'
+})
+export class BackbuttonComponent {
+
+}
---
+++ lms/src/app/shared/component/grid/grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid',
+  imports: [],
+  templateUrl: './grid.component.html',
+  styleUrl: './grid.component.scss'
+})
+export class GridComponent {
+
+}
